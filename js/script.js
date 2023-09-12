10ac0752425af5669897defba7bf2f17c77ca314
Fatoumata SOUKOUNA <soukounafatoumata86@gmail.com>
2023-09-12 22:29:14 +0200

    burger menu p1

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ yearEl.textContent = currentYear;
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".main-nav-list");
+const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
